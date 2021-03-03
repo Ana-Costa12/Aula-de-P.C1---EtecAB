9ac15b83225eb29015da64c9fa90a900dd65d5bf
Ana-Costa12 <claudiasantborges@gmail.com>
2021-03-03 18:39:38 -0300

    Letra de Warriors/Teste de versionamento

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Aula_de_P.C1___EtecAB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkMagenta;
+            Console.WriteLine("--------------------------------------------------------");
+            Console.WriteLine("Warriors (She-Ra and the Princesses of Power Theme Song)");
+            Console.WriteLine("--------------------------------------------------------");
+            Console.WriteLine("Aaliyah Rose");
+            Console.ResetColor();
+            Console.WriteLine();
+            Console.WriteLine(" We're warriors\n Unstoppable, ooh");
+            Console.WriteLine();
+            Console.WriteLine(" We feel the evil coming\n And shadows all around\n Danger surrounds us, but won't bring us down");
+            Console.WriteLine();
+            Console.WriteLine(" We're on the edge of greatness\n Turning darkness to light\n We're right beside you\n Ready to fight");
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.WriteLine(" We must be strong (we must be strong)\n And we must be brave (we must be brave)\n We gotta find every bit of strength\n That we have and never let it go");
+            Console.ResetColor();
+            Console.WriteLine();
+            Console.WriteLine(" We're bound to the struggle\n With mighty sword and flame\n We'll never fail you\n When you call our name");
+            Console.WriteLine();
+            Console.WriteLine(" Together we'll be heroes\n Joining forces as one\n Strong as the steel we carry\n We rise like the sun");
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.WriteLine(" We must be strong (we must be strong)\n And we must be brave (we must be brave)\n We gotta find every bit of strength\n That we have and never let it go\n We must be strong");
+            Console.ResetColor();
+            Console.WriteLine(" Cause we're warriors\n We are unstoppable\n Nothing's…");
+        }
+    }
+}
